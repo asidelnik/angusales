e83a80cbabce3259f7c12a69779b8cbb6cb20297
Amos <asidelnik@gmail.com>
2018-06-13 13:13:09 +0300

    Server - editing

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/Angusales')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 
