e8606d2df5f074564066d90e9986e80ad9a19f1c
Amos Sidelnik <asidelnik@gmail.com>
2018-07-18 13:40:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,11 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
-
-
-// Mongoose
-// mongoose.connect('mongodb://localhost/wordsDB', function () {
-//     console.log("DB connection established!!!");
-// })
-// var Word = require('./models/wordModel');
-
-
 
 // Get our API routes
 const companyAPI = require('./server/routes/company-api'); // ('./server/routes/api');
 const customerAPI = require('./server/routes/customer-api');
 const commentAPI = require('./server/routes/comment-api');
-//...
 
 const app = express();
 
@@ -37,55 +26,22 @@ app.use('/company-api', companyAPI);
 app.use('/customer-api', customerAPI); // use enables the midddleware, which is customerAPI
 app.use('/comment-api', commentAPI);
 
-// app.get('/test', (req, res) => {
-//     res.send('test');
-// })
-
-// app.post('/test', (req, res) => {
-//     res.send('test');
-// })
-
 // Catch all other routes and return the index file
 app.all('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/Angusales/index.html'));      //'dist/index.html'
 });
 
-
-
-/**
- * Get port from environment and store in Express.
- */
+// Get port from environment and store in Express.
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500).send('Something broke!')
 })
 
-
-
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
+// Create HTTP server
 const server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+// Listen on provided port, on all network interfaces.
 server.listen(port, () => console.log(`API running on localhost:${port}`));
-
-
-
-
-/*
-app.get('/test', (req, res) => {
-    res.send('test');
-     
-        1. Call commnet-model get rows method with await to get a promise
-        2. Return the data with res.send
-    
-
-    // res.send(JSON.stringify("Here I am.   you like a hurricane."));
-})
-*/
